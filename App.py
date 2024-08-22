fb315338a960f2db780fb349f123b0ad9b2fbc18
PRO X <bredalisgautreaux@gmail>
2024-08-22 21:27:22 +0200

    feat: ✨ Se le agregó Flask

---
+++ App.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template
+
+def pagina_lyrics():
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+    @app.errorhandler(404)
+    def error_404(e):
+        return render_template("404.html"), 404
+
+    return app
+
+if __name__ == "__main__":
+    app = pagina_lyrics()
+    app.run()
